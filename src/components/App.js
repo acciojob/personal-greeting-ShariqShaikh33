ff4fb677ccc0f7c674a8a927cccb48c14fd7f599
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-12-19 19:42:56 +0530

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,27 @@
 import React from "react";
 import './../styles/App.css';
 
+const namechanged = () =>{
+  let input = document.getElementById("nameinput");
+  let output = document.getElementById("outputdiv");
+  output.innerHTML=input.value;
+  
+}
+
+
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div id="mainwrapper">
+      <label for="nameinput">Enter your name: </label>
+      <input id="nameinput" type="text" onChange={namechanged}></input>
+      <div>
+          Hello <div id="outputdiv" ></div>!
+      </div>
     </div>
   )
 }
 
+
+
 export default App
+
